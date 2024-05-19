81e0bccaa1ffc5402371fa50d61ada6969427dad
BrianRitchie1 <rit20002@byui.edu>
2024-05-18 20:00:46 -0600

    I tried to no avail

--- coolspics/coolpics.js
+++ coolspics/coolpics.js
@@ -1,4 +1,24 @@
+const menuButton = document.querySelector(".menu-button");
+
+function toggleMenu() {
+    const menu = document.querySelector(".menu");
+    menu.classList.toggle("hide");
+}
+
+menuButton.addEventListener("click", toggleMenu);
+
 const year = document.querySelector('#year');
 const currentYear = new Date().getFullYear();
-
 year.textContent = currentYear;
+
+function handleResize() {
+    const menu = document.querySelector(".menu");
+    if (window.innerWidth > 1000) {
+      menu.classList.remove("hide");
+    } else {
+      menu.classList.add("hide");
+    }
+  }
+  
+  handleResize();
+  window.addEventListener("resize", handleResize);
