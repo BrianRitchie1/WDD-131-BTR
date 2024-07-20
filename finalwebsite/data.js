b43faa43ee28da0be532eb15592a60a8edd75f12
BrianRitchie1 <rit20002@byui.edu>
2024-07-19 21:00:10 -0600

    images n stuff

--- finalwebsite/data.js
+++ finalwebsite/data.js
@@ -8,13 +8,13 @@ export const memes = [
     },
     {
         name: "Five Nights at Feddy's (FNAF) Rizz",
-        image: "images/chica-rizz-fnaf-rizz.gif",
+        image: "images/chicka.png",
         number: 2,
         description: "This meme orginates from a video of a man trying to be sexy, on to be animated into the animatrioncs from the popular game FNAF. The irony is in the fact that the animatronics are trying to kill you, not attact you. While the most popular version of this meme is with Chica, it can be mixed with any of the animatronics."
     },
     {
         name: "Gangdom Style Baby",
-        image: "images/Gangdom-syle-baby.gif",
+        image: "images/gangnambaby.jpg",
         number: 3,
         description: "This meme is a twist meme, where instead of having it's first steps, it goes into a complicated K-pop dance. This meme came about do to the rise in popluatity of the song Gandom Style."
     }
